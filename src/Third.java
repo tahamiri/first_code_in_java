057d60f2e59086eb974b34b3a9a431ffe3e96118
tahamiri <tahamiri02@gmail.com>
2024-01-07 13:15:19 +0330

    charAt, array...

--- src/Third.java
+++ src/Third.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Third {
     public static void main(String[] args) {
 
+        char[] array2 = {'a','s','d'};
+
         String x = "hello world";
 
         Character[] array = new Character[x.length()];
@@ -16,7 +18,7 @@ public class Third {
 
         for (int i = 0; i < z; i++){
             System.out.println(array[i]);
-            
+
         }
     }
 }
